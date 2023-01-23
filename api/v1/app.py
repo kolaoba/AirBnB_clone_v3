fc677d6b7fe3a13bd5bb60826de9689c547f4ab1
kolaoba <kolaobajuluwa@gmail.com>
2023-01-23 10:48:01 +0100

    add cors to app.yt

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,14 +3,17 @@
 Flask App that integrates with AirBnb static HTML Template
 """
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
 
 
 @app.teardown_appcontext
